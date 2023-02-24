96c38f5d0cb1c6bd73dcfe0ce88bcc1f62cb2c47
DocSkullka <docskullka@gmail.com>
2023-02-24 12:44:13 +0300

    init: init project

---
+++ src/pages/AboutPage.tsx
@@ -0,0 +1,5 @@
+export function AboutPage() {
+    return (
+        <div>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugiat error aperiam exercitationem dolorem vitae nihil suscipit omnis eum doloribus aliquam ex neque illum tempora illo molestias totam vero maiores nemo rem blanditiis nisi ipsam, consequatur officia labore! Assumenda, eligendi officiis sunt harum alias quam, magnam nostrum, mollitia minima vitae doloribus.</div>
+    )
+}
